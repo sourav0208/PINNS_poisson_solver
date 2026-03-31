389b7b9fadc8217f33b816a868c6566de65f7aa9
sourav0208 <sourav@tum.de>
2026-03-31 18:56:30 +0200

    Add PINN collocation sampling and autograd based PDE and boundary losses

---
+++ src/losses.py
@@ -0,0 +1,52 @@
+import torch 
+import torch.nn as nn
+
+mse_loss = nn.MSELoss()
+
+def compute_pde_residual(model, interior_points):
+
+    u = model(interior_points)
+
+    grads = torch.autograd.grad(
+        outputs = u,
+        inputs = interior_points,
+        grad_outputs = torch.ones_like(u),
+        create_graph=True,#
+        retain_graph=True
+    )[0]
+
+    u_x = grads[:,0:1]
+    u_y = grads[:,1:2]
+
+    grad_x = torch.autograd.grad(
+        outputs=u_x,
+        inputs=interior_points,
+        grad_outputs=torch.ones_like(u_x),
+        create_graph=True,
+        retain_graph=True
+    )[0]
+
+    grad_y = torch.autograd.grad(
+        outputs=u_y,
+        inputs=interior_points,
+        grad_outputs=torch.ones_like(u_y),
+        create_graph=True,
+        retain_graph=True
+    )[0]
+
+    u_xx = grad_x[:,0:1]
+    u_yy = grad_y[:,1:2]
+
+    residual = -(u_xx + u_yy) -1.0
+
+    return residual
+
+def pde_loss(model, interior_points):
+    residual = compute_pde_residual(model, interior_points)
+    target = torch.zeros_like(residual)
+    return mse_loss(residual, target)
+
+def boundary_loss(model, boundary_points):
+    u_boundary = model(boundary_points)
+    target = torch.zeros_like(u_boundary)
+    return mse_loss(u_boundary, target)
